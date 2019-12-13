82fd3409ae0e6a115b9b2c24d59cbf466a70706d
HiWiSciFi <max.j.hager@gmail.com>
2019-12-13 10:02:15 +0100

    Uploaded
    uploaded project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Controller controller;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        int ID = 0;
+        if (!NetworkCommunication.Connect("localhost", 4444, 1, ref ID))
+        {
+            Debug.LogError("Connection Failed");
+        }
+
+        NetworkCommunication.localPlayer = controller;
+    }
+
+    private void Update()
+    {
+        if (NetworkCommunication.DataAvailable)
+            NetworkCommunication.ReadData();
+    }
+}
---
+++ Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,4 @@
+﻿public static class PlayerSettings
+{
+    public static float mouseSpeed = 2.0f;
+}
