037f22e81e24fce1fb982afca3689c1cd770e8eb
HiWiSciFi <max.j.hager@gmail.com>
2019-12-15 11:53:03 +0100

    MainMenu Scene added

---
+++ Assets/Scripts/Menu/ResManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+}
