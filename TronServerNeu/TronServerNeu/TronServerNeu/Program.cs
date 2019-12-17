58dee82527d8f716e1dacca4d2eb8e20e02d5e8d
imurnaili <54451121+imurnaili@users.noreply.github.com>
2019-12-17 22:20:31 +0100

    Update Program.cs

--- TronServerNeu/TronServerNeu/TronServerNeu/Program.cs
+++ TronServerNeu/TronServerNeu/TronServerNeu/Program.cs
@@ -3,13 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace TronServerNeu
 {
     class Program
     {
+        private static  IPEndPoint ipEp;
+        private static  byte version;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args">
+        /// erstes Argument listenport:<port>
+        /// zweites Argument gameversion:<version>
+        /// </param>
         static void Main(string[] args)
         {
+            ProcessArgs(args);
+
+            
+            
+        }
+
+
+        /// <summary>
+        /// Einlesen der Argumente ipEp und version werden gesetzt
+        /// </summary>
+        /// <param name="args">
+        /// erstes Argument listenport:<port>
+        /// zweites Argument gameversion:<version>
+        /// </param>
+        private static void ProcessArgs(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].ToLower().StartsWith("listenport:"))
+                {
+                    try
+                    {
+                        string[] split = args[i].Split(':');
+
+                        ipEp = new IPEndPoint(IPAddress.Any,int.Parse(split[1]));
+                        
+                        continue;
+                    }
+                    catch (Exception e){
+                        Console.WriteLine(e);
+                    }
+                }
+
+                if (args[i].ToLower().StartsWith("gameversion:"))
+                {
+                    try
+                    {
+                        string[] split = args[i].Split(':');
+                        version = byte.Parse(split[1]);
+                        continue;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
         }
     }
 }
