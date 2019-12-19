0c6c15a4929c4df8435a3f9416cd3cf6202ba780
HiWiSciFi <max.j.hager@gmail.com>
2019-12-19 22:01:08 +0100

    things

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject PlayerPrefab;
+    public List<PlayerController> pcs = new List<PlayerController>();
 
-    private void OnEnable()
+    public void OnEnable()
     {
         StartCoroutine(InitializeScene());
     }
@@ -33,9 +35,70 @@ public class GameManager : MonoBehaviour
             player = Instantiate(PlayerPrefab);
             pc = player.GetComponent<PlayerController>();
             pc.Initialize(false, new Color(buffer[2], buffer[3], buffer[4]), buffer[1]);
+            pcs.Add(pc);
         }
         yield return null;
 
         // all players added
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (newNetworkCommunication.DataAvailable)
+        {
+            byte[] buffer = newNetworkCommunication.Receive();
+            if (buffer[0] == 0)
+            {
+                // standard
+                byte ID = buffer[1];
+                float yAngle = BitConverter.ToSingle(buffer, 2);
+                float posX = BitConverter.ToSingle(buffer, 6);
+                float posZ = BitConverter.ToSingle(buffer, 10);
+                byte boosted = buffer[14];
+
+                for (int i = 0; i < pcs.Count; i++)
+                {
+                    if (pcs[i].ID == ID)
+                    {
+                        pcs[i].transform.position = new Vector3(posX, transform.position.y, posZ);
+                        pcs[i].transform.localScale = new Vector3(pcs[i].transform.localScale.x, yAngle, pcs[i].transform.localScale.z);
+                        pcs[i].boosted = boosted;
+                    }
+                }
+            }
+            else if (buffer[0] == 3)
+            {
+                byte ID = buffer[1];
+
+                // kill
+                for (int i = 0; i < pcs.Count; i++)
+                {
+                    if (pcs[i].ID == ID)
+                    {
+                        pcs[i].dead = true;
+                        pcs.RemoveAt(i);
+                    }
+                }
+            }
+            else if (buffer[0] == 4)
+            {
+                byte ID = buffer[1];
+
+                // disconnect
+                for (int i = 0; i < pcs.Count; i++)
+                {
+                    if (pcs[i].ID == ID)
+                    {
+                        pcs[i].dead = true;
+                        pcs.RemoveAt(i);
+                    }
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : NetworkBehaviour {
 
 	// general variables
 	public byte ID = 0;
-	public bool dead { get { return dead; } set { dead = value; StopCoroutine(sendData()); } }
+	private bool _dead = false;
+	public bool dead { get { return _dead; } set { _dead = value; StopCoroutine(sendData()); } }
 
 	// movement variables
 	private const float movingSpeed = 5.0f;
@@ -21,7 +22,6 @@ public class PlayerController : NetworkBehaviour {
 	// mouse variables
 	private float rotY = 0f;
 	private const float rotationStrength = 100.0f;
-	// private Quaternion q = new Quaternion(0, 0, 0, Quaternion.Identity);
 	
 	private void Start() {
 		
@@ -100,11 +100,6 @@ public class PlayerController : NetworkBehaviour {
 
 	public void setColor(Color color)
 	{
-		/*Material m = new Material(GetComponentInChildren<MeshRenderer>().sharedMaterial.shader);
-		m.name = "generated material";
-		m.CopyPropertiesFromMaterial(GetComponentInChildren<MeshRenderer>().sharedMaterial);
-		m.SetColor("_Color", color);
-		GetComponentInChildren<MeshRenderer>().sharedMaterial = m;*/
 		MaterialPropertyBlock block = new MaterialPropertyBlock();
 		block.SetColor("_BaseColor", color);
 		GetComponentInChildren<MeshRenderer>().SetPropertyBlock(block);
